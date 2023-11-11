8ba5603ec0492c11a825e07bd944ab7d2c14e58c
Thomas Tumiel <ttumiel@gmail.com>
2023-11-11 22:47:33 +0000

    logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,28 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, HStack } from "@chakra-ui/react";
 import DayNight from "./DayNight";
+import Image from 'next/image';
 
 const Layout = ({ children }) => {
   return (
     <Box position="relative" minH="100vh">
       <Flex align="flex-start" justify={"space-between"} px={12} pt={8}>
-        <Box>
-          <Heading as="h1" size="lg">
-            Watch and Learn
-          </Heading>
-          <Text fontSize="sm" fontStyle="oblique">Converting Lab Demos to Shareable Protocols.</Text>
-        </Box>
+        <HStack>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={76}
+            height={76}
+            layout="fixed"
+          />
+          <Box>
+            <Heading as="h1" size="lg">
+              Watch and Learn
+            </Heading>
+            <Text fontSize="sm" fontStyle="oblique">
+              Converting Lab Demos to Shareable Protocols.
+            </Text>
+          </Box>
+        </HStack>
         <DayNight />
       </Flex>
       <Box>{children}</Box>
